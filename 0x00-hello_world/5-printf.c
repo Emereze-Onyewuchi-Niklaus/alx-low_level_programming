d7e8458150b8a7de459c06e6d2fc2e95f470f949
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-08 21:54:28 -0700

    Using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * description: Using printf function
+ *
+ * function main: prints a string in the output using printf function
+ *
+ * Return: 0 if exited properly, -1 if otherwise
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

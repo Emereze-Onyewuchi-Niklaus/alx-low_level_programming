3b54df743fc2fde65fffd828ea0440c83c330944
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-08 21:36:43 -0700

    using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * A simple C program to print a string using the puts function.
+ *
+ * main - prints string \"Programing is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, -1 otherwise
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

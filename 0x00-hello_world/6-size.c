a091650787e3f8072dd30e40b42418b80d3d8657
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-11 14:33:19 -0700

    Intel Syntax

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Data types sizes from the sizeof function
+ *
+ * Description: main function prints sizes of Data Types
+ * with the sizeof function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
+	return (0);
+}

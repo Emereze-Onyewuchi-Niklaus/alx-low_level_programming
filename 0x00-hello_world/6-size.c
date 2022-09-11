2c7d6f4bc5251fb175c73c94a590f0c47d58fbe4
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-11 15:00:04 -0700

    modified size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
+	printf("Size of a float: %ld bytes(s)\n", sizeof(f));
 	return (1);
 }

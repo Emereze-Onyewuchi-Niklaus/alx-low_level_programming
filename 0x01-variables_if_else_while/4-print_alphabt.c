d31fe0147f039c6ad6bebcab2ba5fdfc79257d93
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-12 16:25:29 -0700

    Print without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of alphabets using the putchar function
+ * after the program executes
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

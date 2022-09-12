5884198901b23f2b74abe0443619999052fdccda
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-12 02:02:52 -0700

    modified 6-size.C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: main function prints sizes of Data Types
  * with the sizeof function
- * Return: 1
+ * Return: 0
  */
 int main(void)
 {
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %ld bytes(s)\n", sizeof(f));
-	return (1);
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }

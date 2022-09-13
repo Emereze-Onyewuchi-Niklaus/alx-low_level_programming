cd28a9a5cfcb1aabc89fd75c1c66044f977a90ac
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-12 17:16:24 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of numbers using the putchar function
+ * after the program executes
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 48; r <= 58; r++)
+		putchar(r);
+	putchar('\n');
+	return (0);
+}

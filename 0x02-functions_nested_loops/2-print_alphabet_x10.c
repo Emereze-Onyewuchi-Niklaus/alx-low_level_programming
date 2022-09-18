9026f7ace7bd01e6cdb0ab36d9397d523eac6bef
Emereze Onyewuchi <emerezeonyewuchi7@outlook.com>
2022-09-17 19:58:09 -0700

    printing the alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,16 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i = 0; i < 10;)
+	i = 0;
+	while (i < 0)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-		_putchar(ch);
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
 }
